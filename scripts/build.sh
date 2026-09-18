2d75f020e0a30e9e950dd232108d78452c53ab30
Volodymyr Semenyakin <volodymyr.semenyakin@fracturedbyte.com>
2026-09-18 14:35:22 +0300

    - Updated page size in build script for Pi (4096 -> 16384)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 CRYPT_K="0x$(od -An -N8 -tx1 /dev/urandom | tr -d ' \n')" \
   cargo build --release --features harden
 strip target/release/re_protection_test
-cargo run -p crypt --bin seal -- target/release/re_protection_test --page-size 4096
+cargo run -p crypt --bin seal -- target/release/re_protection_test --page-size 16384
